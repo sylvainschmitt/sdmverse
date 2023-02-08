b1784e5d109a1891edcd1a98ed316958d1e55f96
sylvainschmitt <sylvain.schmitt@agroparistech.fr>
2023-02-08 09:18:33 -0300

    fixed yaml

--- R/list_packages.R
+++ R/list_packages.R
@@ -1,4 +1,8 @@
 #' @include utils-pipe.R
+#' @importFrom gh gh
+#' @importFrom yaml read_yaml
+#' @importFrom tibble as_tibble
+#' @importFrom dplyr bind
 NULL
 
 #' List sdmverse packages
@@ -13,5 +17,12 @@ NULL
 #' @export
 #'
 list_packages <- function(){
-  stop("list_packages not developed yet.")
+  request <- gh("GET /repos/sylvainschmitt/sdmverse/git/trees/main?recursive=1")
+  files <- vapply(request$tree, "[[", "", "path")
+  packages <- grep("inst/extdata/packages/", files, value = TRUE, fixed = TRUE)
+  packages <- lapply(packages, function(p) paste0("https://raw.githubusercontent.com/sylvainschmitt/sdmverse/main/", p))
+  lapply(packages, read_yaml) %>%
+    lapply(as_tibble) %>%
+    bind_rows() %>%
+    return()
 }
