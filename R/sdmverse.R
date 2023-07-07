d278d9d46a9ec3f72c7fc92930837486ddd38baa
Adam B. Smith <adam@earthskysea.org>
2023-07-07 10:34:24 -0500

    Create sdmverse.R

---
+++ R/sdmverse.R
@@ -0,0 +1,20 @@
+#' sdmverse: A Meta-Package For Harnessing The Growing Diversity Of Species Distribution Modeling Packages
+#'
+#' \code{sdmverse} is an annotated catalog of R packages for modeling species distributions and ecological niches. Metadata on each package is contributed by the package author(s). Please see \href(https://github.com/sylvainschmitt/sdmverse}{instructions} for how to contribute or update your package.
+#'
+#' Create an issue on \href{https://github.com/sylvainschmitt/sdmverse/issues}{GitHub}.
+#'
+#' @details
+#' @section Functions
+#' \code{check_cran_metadata}: Check package metdadata with the metadata on CRAN
+#' \code{launch_app}: Launch the \code{sdmverse} Shiny app
+#' \code{list_packages}: Fetch a list of packages from the online \code{sdmverse} repository
+#' \code{plot_dendrogram}: Cluster diagrams of packages based on their functionality
+#' \code{plot_table}: Table of functionality of packages'
+#' \code{template_from_cran}: Create a \code{yaml} template for a package from CRAN metadata
+#' 
+#' @docType package
+#' @name sdmverse
+#' @keywords internal 
+"_PACKAGE"
+NULL
